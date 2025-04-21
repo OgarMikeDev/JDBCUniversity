f16bb1e66dcc45c0d46feae69eaaa64e43897099
ogar_m <ogar.m.v@maio.ru>
2025-04-21 14:25:24 +0300

    jdbs

---
+++ src/main/java/Course.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private int id;
+    private String name;
+    private int duration;
+    private CourseType type;
+    private String description;
+    private int teacherId;
+    private int studentsCount;
+    private double price;
+    private double pricePerHour;
+    private static List<Course> listAllCourses = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public CourseType getType() {
+        return type;
+    }
+
+    public void setType(CourseType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getStudentsCount() {
+        return studentsCount;
+    }
+
+    public void setStudentsCount(int studentsCount) {
+        this.studentsCount = studentsCount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public static void addCourse(Course course) {
+        listAllCourses.add(course);
+    }
+
+    public static void getListAllCourses() {
+        for (Course currentCourse : listAllCourses) {
+            System.out.println("\"" + currentCourse + "\"");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", duration=" + duration +
+                ", type=" + type +
+                ", description='" + description + '\'' +
+                ", teacherId=" + teacherId +
+                ", studentsCount=" + studentsCount +
+                ", price=" + price +
+                ", pricePerHour=" + pricePerHour +
+                '}';
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        String user = "root";
+        String url = "jdbc:mysql://localhost:3306/my_database";
+        String password = "Misha06122000)))";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM courses");
+            while (resultSet.next()) {
+                Course currentCourse = new Course();
+                currentCourse.setId(resultSet.getInt("id"));
+                currentCourse.setName(resultSet.getString("name"));
+                currentCourse.setDuration(resultSet.getInt("duration"));
+                currentCourse.setType(CourseType.valueOf(resultSet.getString("type")));
+                currentCourse.setDescription(resultSet.getString("description"));
+                currentCourse.setTeacherId(resultSet.getInt("teacher_id"));
+                currentCourse.setStudentsCount(resultSet.getInt("students_count"));
+                currentCourse.setPrice(resultSet.getDouble("price"));
+                currentCourse.setPricePerHour(resultSet.getDouble("price_per_hour"));
+                Course.addCourse(currentCourse);
+            }
+
+            resultSet = statement.executeQuery("SELECT * FROM teachers");
+            while (resultSet.next()) {
+                Teacher teacher = new Teacher();
+                teacher.setId(resultSet.getInt("id"));
+                teacher.setName(resultSet.getString("name"));
+                teacher.setSalary(resultSet.getDouble("salary"));
+                teacher.setAge(resultSet.getInt("age"));
+                Teacher.addTeacher(teacher);
+            }
+
+            System.out.println("Все курсы:");
+            Course.getListAllCourses();
+            System.out.println("Все учителя:");
+            Teacher.getListAllTeachers();
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception ex) {
+            ex.getMessage();
+        }
+
+    }
+}
