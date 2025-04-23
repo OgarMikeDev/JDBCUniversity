616972c9d32c7008ed4b7e12c2e5c50a81e5976b
ogar_m <ogar.m.v@maio.ru>
2025-04-23 09:08:55 +0300

    university

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
         String user = "root";
-        String url = "jdbc:mysql://localhost:3306/my_database";
+        String url = "jdbc:mysql://localhost:3306/university";
         String password = "Misha06122000)))";
 
         try {
